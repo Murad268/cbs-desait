ee7625b7132814e57d2b5da864a0b24b23fcbabd
Murad <agamedov94@mail.ru>
2023-08-01 21:03:11 +0400

    services2

--- app/Http/Controllers/admin/BlogsController.php
+++ app/Http/Controllers/admin/BlogsController.php
@@ -30,20 +30,12 @@ class BlogsController extends Controller
     public function store(CreateBlogRequest $request)
     {
         $imageService = app(İmageService::class);
-
         $result = $imageService->downloadImage($request->card_img, 'assets/front/images/');
         $result1 = $imageService->downloadImage($request->card_banner, 'assets/front/images/');
-
-
-
-
-
-
         $category_id = $request->category_id;
         $blog_title = $request->blog_title;
         $blog_content = $request->blog_content;
         $elems = ["category_id" => $category_id, "card_img" => $result, 'blog_title' => $blog_title, 'blog_content' => $blog_content, 'card_banner' => $result1];
-
         try {
             Blog::create($elems);
             return redirect()->route('admin.blogs.index');
@@ -63,65 +55,21 @@ class BlogsController extends Controller
 
     public function update(UpdateBlogRequest $request, $id)
     {
-
-
-
-        // Find the blog record
         try {
             $blog = Blog::findOrFail($id);
-
-            $blog->category_id = $request->category_id;
-            $blog->blog_title = $request->blog_title;
-            $blog->blog_content = $request->blog_content;
-
-
-            if ($request->hasFile('card_img')) {
-                // Delete old image if exists
-                $this->deleteImage($blog->card_img);
-
-                // Save new image
-                $blog->card_img = $this->saveImage($request->file('card_img'));
-            } else {
-                $blog->card_img = $blog->card_img;
-            }
-
-            if ($request->hasFile('card_banner')) {
-
-                $this->deleteImage($blog->card_banner);
-
-
-                $blog->card_banner = $this->saveImage($request->file('card_banner'));
-            } else {
-                $blog->card_banner = $blog->card_banner;
-            }
-
-            $blog->save();
-
+            $imageService = app(İmageService::class);
+            $result = $imageService->updateImage($request, 'assets/front/images/', 'card_img',  $request->card_img , $blog->card_img );
+            $result1= $imageService->updateImage($request, 'assets/front/images/', 'card_banner',  $request->card_banner , $blog->card_banner );
+            $category_id = $request->category_id;
+            $blog_title = $request->blog_title;
+            $blog_content = $request->blog_content;
+            $elems = ["category_id" => $category_id, "card_img" => $result, 'blog_title' => $blog_title, 'blog_content' => $blog_content, 'card_banner' => $result1];
+            $blog->update($elems);
             return redirect()->route('admin.blogs.index');
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'An error occurred while updating the blog');
         }
     }
-
-    private function saveImage($image)
-    {
-        $extension = $image->getClientOriginalExtension();
-        $randomName = Str::random(10);
-        $imagePath = 'assets/front/images/';
-        $lastName = $randomName . "." . $extension;
-        $image->move($imagePath, $lastName);
-
-        return $imagePath . $lastName;
-    }
-
-    private function deleteImage($imagePath)
-    {
-        if (File::exists(public_path($imagePath))) {
-            File::delete(public_path($imagePath));
-        }
-    }
-
-
 
     public function destroy($id) {
         try {
