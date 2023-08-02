6dd7aa883dad2b1a0cf9ebfe8067f30df5907c97
Murad <agamedov94@mail.ru>
2023-08-03 00:06:33 +0400

    res

--- resources/views/components/front-navbar-component.blade.php
+++ resources/views/components/front-navbar-component.blade.php
@@ -5,11 +5,11 @@
                   <img src="{{asset('assets/front/icons/LOGO.svg')}}" alt="">
                </a>
                <ul class="header__navbar__list">
-                  <li><a href="">Əsas Səhifə</a></li>
-                  <li><a href="">Haqqımızda</a></li>
+                  <li><a href="{{route('front.index')}}">Əsas Səhifə</a></li>
+                  <li><a href="{{route('front.about')}}">Haqqımızda</a></li>
                   <li class="our__services"><span>Xidmətlərimiz</span><i class="fa fa-angle-down nav__down" aria-hidden="true"></i></li>
-                  <li><a href="">Portfolio</a></li>
-                  <li><a href="">Bloq</a></li>
+                  <li><a href="{{route('front.portfolio')}}">Portfolio</a></li>
+                  <li><a href="{{route('front.blogs')}}">Bloq</a></li>
                </ul>
                <a href="" class="btn__withoutBg">BİZDƏN GÖRÜŞ AL</a>
                <div class="header__hamburger">
