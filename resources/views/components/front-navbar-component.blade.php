4c17a9935236def29acc2be3c6da92e531b293fe
Murad <agamedov94@mail.ru>
2023-08-03 23:48:04 +0400

    res

--- resources/views/components/front-navbar-component.blade.php
+++ resources/views/components/front-navbar-component.blade.php
@@ -5,11 +5,13 @@
                 <img src="{{asset('assets/front/icons/'.$settings->logo)}}" alt="">
             </a>
             <ul class="header__navbar__list">
-                <li><a href="{{route('front.index')}}">Əsas Səhifə</a></li>
-                <li><a href="{{route('front.about')}}">Haqqımızda</a></li>
-                <li class="our__services"><span>Xidmətlərimiz</span><i class="fa fa-angle-down nav__down" aria-hidden="true"></i></li>
-                <li><a href="{{route('front.portfolio')}}">Portfolio</a></li>
-                <li><a href="{{route('front.blogs')}}">Bloq</a></li>
+                <li><a class="{{ request()->routeIs('front.index') ? 'active' : '' }}" href="{{ route('front.index') }}">Əsas Səhifə</a></li>
+                <li><a class="{{ request()->routeIs('front.about') ? 'active' : '' }}" href="{{ route('front.about') }}">Haqqımızda</a></li>
+                <li class="our__services {{ request()->routeIs('front.services', 'front.services.*') ? 'active' : '' }}">
+                    <span>Xidmətlərimiz</span><i class="fa fa-angle-down nav__down" aria-hidden="true"></i>
+                </li>
+                <li><a class="{{ request()->routeIs('front.portfolio') ? 'active' : '' }}" href="{{ route('front.portfolio') }}">Portfolio</a></li>
+                <li><a class="{{ request()->routeIs('front.blogs') ? 'active' : '' }}" href="{{ route('front.blogs') }}">Bloq</a></li>
             </ul>
             <a href="" class="btn__withoutBg">BİZDƏN GÖRÜŞ AL</a>
             <div class="header__hamburger">
@@ -18,4 +20,4 @@
         </div>
     </div>
 </nav>
-<x-header-services-component/>
+<x-header-services-component />
